5a017fffb9ffecb53743a10ebe21a1a98d32b7aa
Philip Kheav <philip.kheav@hotmail.com>
2024-10-28 15:43:27 +1100

    remove incompatible rails feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,4 @@ class ApplicationController < ActionController::Base
   include SessionsHelper
 
   before_action :authenticate_user
-
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
 end
