7f8fcacca6de1330371c0574c860ce2d9b6c10ed
Philip Kheav <philip.kheav@hotmail.com>
2024-11-11 16:33:21 +1100

    upgrade to Rails 7.2.2

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,9 +2,6 @@ class Transaction < ApplicationRecord
   belongs_to :source_wallet, class_name: "Wallet", optional: true
   belongs_to :target_wallet, class_name: "Wallet", optional: true
 
-
-
-
   validates :amount, presence: true, format: { with: /\A[-+]?\d+(?:\.\d{0,2})?\z/ }, numericality: { other_than: 0 }
   validates :description, length: { in: 2..255 }
 end
